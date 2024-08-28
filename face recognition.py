f61f13a13f38594306faa3e57a855aef1a4e633f
Ananya12web <ananyakhusi12@gmail.com>
2024-08-28 19:51:05 +0530

    Add files via upload

---
+++ face recognition.py
@@ -0,0 +1,21 @@
+import cv2
+face_cap= cv2.CascadeClassifier("D:\haarcascade_frontalface_alt.xml")
+video_cap=cv2.VideoCapture(0)
+while True :
+    ret , video_new = video_cap.read()
+    col = cv2.cvtColor(video_new,cv2.COLOR_BGR2GRAY)
+    faces = face_cap.detectMultiScale(
+        col,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(video_new,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.imshow("video_live" , video_new)
+    if cv2.waitKey(10) == ord("a"):
+        break
+video_cap.release()
+
+    
